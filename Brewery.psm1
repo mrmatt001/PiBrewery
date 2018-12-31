4d38f7d9229bff06ce1503e9383e606e31977412
Matt Gelder <matt_gelder@hotmail.com>
2018-12-31 12:23:49 +0000

    Postgres install

--- Brewery.psm1
+++ Brewery.psm1
@@ -2,5 +2,7 @@ function Install-Postgres
 {
     sudo apt install postgresql libpq-dev postgresql-client postgresql-client-common -y
     sudo su postgres
-    createuser pi -P --interactive
+    $Found = $false
+    foreach ($Line in (psql -c '\l')) { if ($Line -match '^test') {$Found = $true}}
+    $Found
 }
